982655e37c93bff75d2bfaa89a0490d539c7329f
HP <rawan.manswor@gmail.com>
2023-10-20 16:47:58 +0300

    migration

--- database/migrations/2023_10_20_130023_add_user_id_to_table.php
+++ database/migrations/2023_10_20_130023_add_user_id_to_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
         });
     }
-
     /**
      * Reverse the migrations.
      *
